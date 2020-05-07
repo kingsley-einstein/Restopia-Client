5e63b4773ef162f67aa385bf8f1fc74f5887086d
Kingsley Victor <javaprodigy56@gmail.com>
2020-05-07 08:52:08 +0100

    include options for urlencoded and multipart formats

--- src/app/components/form-client/form-client.component.ts
+++ src/app/components/form-client/form-client.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { StorageService } from "../../services";
+import { StorageService, TabsHistoryService } from "../../services";
 // import { RequestModel } from "../../models/request.model";
 
 @Component({
@@ -9,7 +9,7 @@ import { StorageService } from "../../services";
 })
 export class FormClientComponent implements OnInit {
   @Input()
-  tabName: string = "";
+  tabId: string = "";
 
   url: string = "";
   allowedMethods: { name: string; value: string; }[] = [
@@ -20,19 +20,40 @@ export class FormClientComponent implements OnInit {
     { name: "PUT", value: "PUT" },
     { name: "OPTIONS", value: "OPTIONS" }
   ];
+  requestBodyTypes: { name: string; value: string; }[] = [
+    { name: "json", value: "json" },
+    { name: "urlencoded", value: "urlencoded" },
+    { name: "multipart", value: "multipart" }
+  ];
   method: string = "GET";
+  requestBodyType: string = "json";
   response: string = "";
   body: any = "{}";
   headers: any = "{}";
 
+  // Urlencoded keys
+  objects: { key: string; value: string; }[] = [
+    { key: "", value: "" }
+  ];
+
+  // Multipart keys
+  formDataObjects: { key: string; type: string; value: any }[] = [
+    { key: "", type: "text", value: "" }
+  ];
   // @Output()
   // e: EventEmitter<RequestModel[]> = new EventEmitter<RequestModel[]>();
 
-  constructor(private storage: StorageService) {}
+  constructor(private storage: StorageService, private tabHistory: TabsHistoryService) {}
 
   ngOnInit() {
     console.log("[Form]");
-    console.log(this.tabName);
+    console.log(this.tabId);
+    if (!this.tabHistory.getTabContent(this.tabId)) {
+      this.saveTabContent();
+    }
+    if (this.tabHistory.getTabContent(this.tabId)) {
+      this.getTabContent();
+    }
   }
 
   addToLocalStorage() {
@@ -44,11 +65,26 @@ export class FormClientComponent implements OnInit {
   }
 
   async fetchData() {
+    if (this.requestBodyType === "json") {
+      this.body = JSON.stringify(JSON.parse(this.body));
+    }
+    if (this.requestBodyType === "urlencoded") {
+      const newBody = {};
+      this.objects.forEach((o) => {
+        newBody[o.key] = o.value;
+      });
+      this.body = JSON.stringify(newBody, undefined, 2);
+    }
+    if (this.requestBodyType === "multipart") {
+      const formData: FormData = new FormData();
+      this.formDataObjects.forEach((o) => {
+        formData.append(o.key, o.value, (o.value.name ? o.value.name : ""));
+      });
+      this.body = formData;
+    }
     const r = await fetch(this.url, {
       method: this.method,
-      body: (this.method === "GET" || this.method === "HEAD") ? null : JSON.stringify(
-        JSON.parse(this.body)
-      ),
+      body: (this.method === "GET" || this.method === "HEAD") ? null : this.body,
       headers: JSON.parse(this.headers) || null
     });
     const json = await r.json();
@@ -58,6 +94,7 @@ export class FormClientComponent implements OnInit {
   async fetchAndUpdate() {
     await this.fetchData();
     this.addToLocalStorage();
+    this.saveTabContent();
   }
 
   // updateHistory() {
@@ -93,10 +130,77 @@ export class FormClientComponent implements OnInit {
         JSON.parse(event.target.value), undefined, 2
       );
     }, 100);
+    this.saveTabContent();
     event.target.setSelectionRange(cursorPosition, cursorEnd);
   }
 
   logMethod() {
     console.log(this.method);
+    this.saveTabContent();
+  }
+
+  saveTabContent() {
+    setTimeout(() => {
+      this.tabHistory.addTabContent(this.tabId, {
+        url: this.url,
+        method: this.method,
+        requestBodyType: this.requestBodyType,
+        response: this.response,
+        body: this.body,
+        headers: this.headers
+      });
+    }, 500);
+  }
+
+  getTabContent() {
+    const content = this.tabHistory.getTabContent(this.tabId);
+    this.url = content.url;
+    this.method = content.method;
+    this.requestBodyType = content.requestBodyType;
+    this.response = content.response;
+    this.body = content.body;
+    if (this.requestBodyType === "urlencoded") {
+      if (Object.keys(JSON.parse(this.body)).length > 0) {
+        this.objects = [];
+        Object.keys(JSON.parse(this.body)).forEach((key) => {
+        this.objects.push({
+          key, value: JSON.parse(this.body)[key]
+        });
+      });
+      }
+    }
+    this.headers = content.headers;
+  }
+
+  watchUrlencodedFields() {
+    console.log(this.objects);
+    const newBody = {};
+    this.objects.forEach((o) => {
+        newBody[o.key] = o.value;
+      });
+    this.body = JSON.stringify(newBody, undefined, 2);
+    this.saveTabContent();
+  }
+
+  pushUrlEncoded() {
+    this.objects.push({
+      key: "",
+      value: ""
+    });
+  }
+
+  removeUrlEncoded(i: any) {
+    const index = this.objects.indexOf(i);
+    this.objects.splice(index, 1);
+  }
+
+  watchMultipartFields() {
+    const formData: FormData = new FormData();
+    this.formDataObjects.forEach((o) => {
+        formData.append(o.key, o.value, (o.value.name ? o.value.name : ""));
+      });
+    this.body = formData;
+    this.saveTabContent();
+    console.log(this.formDataObjects);
   }
 }
